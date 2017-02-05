5d0850368e54663038ca6c66bb9c72f0eaf18e17
Jorge <jogonzal@microsoft.com>
2017-02-05 14:14:05 -0800

    Small refactor

--- Problem1.py
+++ Problem1.py
@@ -1,21 +1,28 @@
 import numpy as np
 from numpy.linalg import inv
 
+def calculateObjectiveFunction(a, X, y):
+    totalObjectiveFunction = 0;
+    for i in range(0, m):  # For all datapoints
+        individualObjectiveFunction = np.dot(a.T, X[i]) - y[i];
+        individualObjectiveFunction = 0.5 * individualObjectiveFunction * individualObjectiveFunction;
+        totalObjectiveFunction += individualObjectiveFunction;
+    return totalObjectiveFunction;
+
 n = 100 # dimensions of data
 m = 1000 # number of data points
 X = np.random.normal(0,1, size=(m,n))
 a_true = np.random.normal(0,1, size=(n,1))
 y = X.dot(a_true) + np.random.normal(0,0.1,size=(m,1))
 
+## 1.A
+
 # Calculate the most optimal a
-a = np.dot(np.dot(inv(np.dot(X.T, X)), X.T), y);
+closedFormA = np.dot(np.dot(inv(np.dot(X.T, X)), X.T), y);
+closedFormAObjectiveValue = calculateObjectiveFunction(closedFormA, X, y);
 
-# Now calculate the value of the objective function with a
-totalObjectiveFunction = 0;
-for i in range(0, m): # For all datapoints
-    individualObjectiveFunction = np.dot(a.T, X[i]) - y[i];
-    individualObjectiveFunction = 0.5 * individualObjectiveFunction * individualObjectiveFunction;
-    totalObjectiveFunction += individualObjectiveFunction;
+print "Found value of closedFormA " + str(closedFormA)
+print "Error with that a is " + str(closedFormAObjectiveValue);
 
-print "Found value of a " + str(a)
-print "Error with that a is " + str(totalObjectiveFunction);
+## 1.B
+
