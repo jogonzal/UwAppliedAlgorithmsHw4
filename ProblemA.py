a53c3bed924a38b8a55386be77bb431ccfa95fe4
Jorge <jogonzal@microsoft.com>
2017-02-05 14:11:18 -0800

    Solve problem 1a

--- ProblemA.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-n = 100 # dimensions of data
-m = 1000 # number of data points
-X = np.random.normal(0,1, size=(m,n))
-a_true = np.random.normal(0,1, size=(n,1))
-y = X.dot(a_true) + np.random.normal(0,0.1,size=(m,1))
-
